34a4b4ca656647363a94008edb259d52b71483f8
Dmitri Gordeev <dimapushkin@gmail.com>
2021-08-26 00:31:26 +0300

    done with log in logic

--- TwoScreenApp/LogInViewController.swift
+++ TwoScreenApp/LogInViewController.swift
@@ -12,30 +12,50 @@ class LogInViewController: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton! // не уверен нужен ли этот аутлет, точнее не понимаю
     
     @IBOutlet weak var forgotUserNameButton: UIButton!
     @IBOutlet weak var forgotPasswordButton: UIButton!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     @IBAction func logInButtonPressed() {
-    }
-    @IBAction func forgotUNAlert() {
-    }
-    @IBAction func forgotPassAlert() {
+        if userNameTF.text == "Lex" && passwordTF.text == "12345"{
+            return
+        } else {
+            wrongPassOrUserNameAlert(with: "Oopsy !!!", and: "username or password is incorrect")
+        }
     }
     
-
+    @IBAction func forgotUNAlert() {
+        showPromptAlert(with: "Oopsy !!!", and: "Your name is Lex")
+    }
+    
+    @IBAction func forgotPassAlert() {
+        showPromptAlert(with: "Oopsy !!!", and: "The password is 12345")
+        
+    }
+    
+    
 }
 
 // MARK: - Private Methods
 extension LogInViewController {
-    private func showAlert(with title: String, and massege: String) {
+    private func showPromptAlert(with title: String, and massege: String) {
         let alert = UIAlertController(title: title, message: massege, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    private func wrongPassOrUserNameAlert(with title: String, and massege: String) {
+        let alert = UIAlertController(title: title, message: massege, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        alert.addAction(okAction)
         present(alert, animated: true)
     }
     
