a498284f62e0a689384891a1e21f695a2bd25dae
Dmitri Gordeev <dimapushkin@gmail.com>
2021-08-25 16:00:34 +0300

    Initial Commit

---
+++ TwoScreenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoScreenApp
+//
+//  Created by Повелитель on 25.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
